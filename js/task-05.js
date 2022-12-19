a356a04d2dd8f2d3e39d7b7c2e3632b9c5433108
vadrille12 <109307947+vadrille12@users.noreply.github.com>
2022-12-19 19:02:11 +0200

    done 5 and 6 task

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const refs = {
+    input: document.querySelector("#name-input"),
+    output: document.querySelector("#name-output"),
+}
+
+refs.input.addEventListener('input', onInputChange)
+
+function onInputChange(event) {
+    refs.output.textContent = event.currentTarget.value
+}
---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const validationInputRef = document.querySelector("#validation-input");
+
+function checkNumbers () {
+  if (
+    validationInputRef.value.length ===
+    Number(validationInputRef.dataset.length)
+  ) {
+    validationInputRef.classList.remove(`invalid`);
+    validationInputRef.classList.add(`valid`);
+  } else {
+    validationInputRef.classList.remove(`valid`);
+    validationInputRef.classList.add(`invalid`);
+  }
+};
+
+validationInputRef.addEventListener("click", checkNumbers);
