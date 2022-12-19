52a0c907f7d1b6bc9a906a15935b07bdf479fe0e
vadrille12 <109307947+vadrille12@users.noreply.github.com>
2022-12-19 17:43:44 +0200

    done 1 and 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsElement = document.querySelector("#ingredients");
+
+const createItemElement = ingredients.map((el) => {
+  const ingredientsItemsElement = document.createElement("li");
+  ingredientsItemsElement.classList.add("item");
+  ingredientsItemsElement.textContent = el;
+
+  return ingredientsItemsElement;
+});
+
+ingredientsElement.append(...createItemElement);
+console.dir(ingredientsElement);
