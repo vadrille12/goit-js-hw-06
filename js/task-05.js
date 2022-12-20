6bd5f25c9f2887d42fe33aac77b5578bf33be0d2
vadrille12 <109307947+vadrille12@users.noreply.github.com>
2022-12-20 22:35:52 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,14 @@
 const refs = {
-    input: document.querySelector("#name-input"),
-    output: document.querySelector("#name-output"),
-}
+  input: document.querySelector("#name-input"),
+  output: document.querySelector("#name-output"),
+};
 
-refs.input.addEventListener('input', onInputChange)
+refs.input.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
-    refs.output.textContent = event.currentTarget.value
+  if (refs.input.value.length > 0) {
+    refs.output.textContent = event.currentTarget.value;
+  } else {
+      refs.output.textContent = "Anonymous";
+  }
 }
